2e7a20c0dd7ddf3f54cdaa6e90a3e39f3892465a
Wannes Depuydt <depuydtwannes2010@gmail.com>
2025-07-09 12:25:50 +0200

    Bug fix: beam at feat

--- src/main/java/com/wdpserver/wdpcustomitems/GraplingHandeler.java
+++ src/main/java/com/wdpserver/wdpcustomitems/GraplingHandeler.java
@@ -45,7 +45,7 @@ public class GraplingHandeler implements Listener {
                     return;
                 }
 
-                Location playerFeet = player.getLocation().clone().subtract(0, 0.1, 0);
+                Location playerFeet = player.getEyeLocation().clone().subtract(0, 0.1, 0);
                 Location startLocation = playerFeet.clone();
 
                 if (!returning) {
